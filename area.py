df3b4375f3d0e3cf73f5f2ebba91a5914f7f3948
Александр Истратенко <ist.ko.alex@yandex.ru>
2024-05-09 10:14:26 +0300

    add ekz: harry_house, kings_kross

--- area.py
+++ area.py
@@ -2,19 +2,27 @@ from base_class import Base
 from user import User
 
 class Area(Base):
-    name = None
-
-class Muggle(Area):
     
-    office_price = 50
-    firm_price = 4*office_price + 50
-
-    def init(self, name, rent, price, deposite, firm=False, office_count=0):
+    def init(self, name, price, rent, deposite_stock, dep_one_off, dep_two_off, 
+            dep_three_off, dep_four_off, dep_industry, firm=False, office_count=0):
         self.name = name
-        self.rent = rent
         self.price = price
-        self.deposite = deposite
+        self.rent = rent
+        self.deposite = deposite_stock
+        self.dep_one_off = dep_one_off
+        self.dep_two_off = dep_two_off
+        self.dep_three_off = dep_three_off
+        self.dep_four_off = dep_four_off
+        self.dep_industry = dep_industry
         self.firm = firm
         self.office_count = office_count
 
-        
+# Первая отрасль первой стороны
+class First_1(Area):
+    office_price = 50
+    firm_price = 4*office_price + 50
+
+harry_house = First_1(name='Дом Гарри', price=60, rent=2, deposite_stock=30, 
+            dep_one_off=10, dep_two_off=30, dep_three_off=90, dep_four_off=160, dep_industry=250)
+railway_st_kings_kross = First_1(name='Дом Гарри', price=60, rent=2, deposite=30, 
+                        dep_one_off=20, dep_two_off=60, dep_three_off=180, dep_four_off=320, dep_industry=450)
