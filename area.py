96c8e5789a3d85b6ff23e3498292dafb8173994d
Александр Истратенко <ist.ko.alex@yandex.ru>
2024-05-09 13:16:02 +0300

    add methods: buy and dep area

--- area.py
+++ area.py
@@ -21,19 +21,19 @@ class First_1(Area):
     office_price = 50
     firm_price = 4*office_price + 50
 
-brown_one = First_1(name='Дом Гарри', price=60, deposite=30, rent_stock=2, 
-            rent_one_off=10, rent_two_off=30, rent_three_off=90, rent_four_off=160, rent_firm=250)
-brown_two = First_1(name='Вокзал Кингс-Кросс', price=60, deposite=30, rent_stock=4, 
-            rent_one_off=20, rent_two_off=60, rent_three_off=180, rent_four_off=320, rent_firm=450)
+# brown_one = First_1(name='Дом Гарри', price=60, deposite=30, rent_stock=2, 
+#             rent_one_off=10, rent_two_off=30, rent_three_off=90, rent_four_off=160, rent_firm=250)
+# brown_two = First_1(name='Вокзал Кингс-Кросс', price=60, deposite=30, rent_stock=4, 
+#             rent_one_off=20, rent_two_off=60, rent_three_off=180, rent_four_off=320, rent_firm=450)
 
 # Вторая отрасль первой стороны
 class First_2(Area):
     office_price = 50
     firm_price = 4*office_price + 50
 
-blue_one = First_2(name='Отдел магического транспорта', price=100, deposite=50, rent_stock=6, 
-            rent_one_off=30, rent_two_off=90, rent_three_off=270, rent_four_off=400, rent_firm=550)
-blue_two = First_2(name='Отдел тайн', price=100, deposite=50, rent_stock=6, 
-            rent_one_off=30, rent_two_off=90, rent_three_off=270, rent_four_off=400, rent_firm=550)
-blue_three = First_2(name='Отдел обеспечения магического правопорядка', price=120, deposite=60, rent_stock=8, 
-            rent_one_off=40, rent_two_off=100, rent_three_off=300, rent_four_off=450, rent_firm=600)
+# blue_one = First_2(name='Отдел магического транспорта', price=100, deposite=50, rent_stock=6, 
+#             rent_one_off=30, rent_two_off=90, rent_three_off=270, rent_four_off=400, rent_firm=550)
+# blue_two = First_2(name='Отдел тайн', price=100, deposite=50, rent_stock=6, 
+#             rent_one_off=30, rent_two_off=90, rent_three_off=270, rent_four_off=400, rent_firm=550)
+# blue_three = First_2(name='Отдел обеспечения магического правопорядка', price=120, deposite=60, rent_stock=8, 
+#             rent_one_off=40, rent_two_off=100, rent_three_off=300, rent_four_off=450, rent_firm=600)
--- user.py
+++ user.py
@@ -2,33 +2,39 @@ from base_class import Base
 from area import Area
 
 class User(Base):
-    def __init__(self, name, areas=[], budget=2000):
+    def __init__(self, name, areas, budget):
         self.name = name
         self.areas = areas
         self.budget = budget
 
     def buy_area(self, area):
-        if area not in self.areas and self.budget >= brown_one.price:
+        if area not in self.areas and self.budget >= area.price:
             self.areas.append(area)
-            print(f'Игроку {self.name} добавлен участок {area.name}')
+            self.budget = self.budget-brown_one.price
+            print(f"Игроку {self.name} добавлен участок {area.name}. Баланс {self.name} равен {self.budget}.\nВ собственности {self.name} находятся: {self.areas}")
         elif area not in self.areas and self.budget < brown_one.price:
             print(f"Стоимость участка '{area.name}' слишком велика")
         elif area in self.areas:
             print(f'Участок уже принадлежит игроку {self.name}')
 
-    def del_area(self, area):
+    def dep_area(self, area):
         if area in self.areas:
-            self.areas.append(area)
-            print(f'Игроку {self} добавлен участок {area}')
+            print(f'Участок {area.name} закладывается в банк под сумму {area.deposite}')
+            # следует придумать логику закладывания: когда никто кроме заложившего
+            # не может выкупить эту собственность
+            self.areas.remove(area)
+            self.budget += area.deposite
+            print(f"Игроку {self.name} добавлен участок {area}. Баланс {self.name} равен {self.budget}.\nВ собственности {self.name} находятся: {self.areas}")
         else:
-            (f'Участка и так нет во владении')
+            print(f'Участка и так нет во владении')
 
-user_1 = User(name="Саша")
-user_2 = User(name="Настя")
+user_1 = User(name="Саша", areas=[], budget=2000)
+user_2 = User(name="Настя", areas=[], budget=2000)
 
-brown_one = Area(name='Дом Гарри', price=6000, deposite=30, rent_stock=2, 
+brown_one = Area(name='Дом Гарри', price=60, deposite=30, rent_stock=2, 
             rent_one_off=10, rent_two_off=30, rent_three_off=90, rent_four_off=160, rent_firm=250)
 brown_two = Area(name='Вокзал Кингс-Кросс', price=60, deposite=30, rent_stock=4, 
             rent_one_off=20, rent_two_off=60, rent_three_off=180, rent_four_off=320, rent_firm=450)
 
 user_1.buy_area(brown_one)
+user_1.dep_area(brown_one)
